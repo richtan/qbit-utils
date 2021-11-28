b3ea169cf4b64fae442ad820c6ac11407ea153bb
Richie Tan <29845690+richtan@users.noreply.github.com>
2021-11-28 11:24:32 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Qbit Unchecker
+This program is used to stop torrents from rechecking in Qbittorrent, which can take a long time
+
+# Python dependencies:
+- python-dotenv
+- qbittorrent-api
+
+# Usage:
+- Git clone this repository
+- Install Python 3 and place it in your PATH environment variable
+- Install the python dependencies
+- Run main.py with python 3
---
+++ main.py
@@ -0,0 +1,43 @@
+import qbittorrentapi
+from pathlib import Path
+import shutil
+import pprint
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+client = qbittorrentapi.Client(host=config.get("QBIT_URL"))
+
+qbit_appdata_path = Path(config.get("QBIT_APPDATA_PATH"))
+bt_backup_path = qbit_appdata_path / 'BT_backup'
+torrents_backup_path = qbit_appdata_path / 'torrents'
+
+torrents_backup_path.mkdir(parents=True, exist_ok=True)
+
+try:
+    client.auth_log_in()
+except qbittorrentapi.LoginFailed as e:
+    print(e)
+
+checking_torrents = [torrent for torrent in client.torrents_info() if torrent.state_enum.is_checking]
+for torrent in checking_torrents:
+    pprint.pprint(dict(torrent), indent=4)
+    torrent_path = torrents_backup_path / f"{torrent.hash}.torrent"
+    shutil.copy(bt_backup_path / f"{torrent.hash}.torrent", torrent_path)
+    client.torrents_delete(torrent_hashes=torrent.hash)
+    client.torrents_add(
+            torrent_files=str(torrent_path),
+            save_path=torrent.save_path,
+            category=torrent.category,
+            is_skip_checking=True,
+            rename=torrent.name,
+            upload_limit=torrent.up_limit,
+            download_limit=torrent.dl_limit,
+            use_auto_torrent_management=torrent.auto_tmm,
+            is_sequential_download=torrent.seq_dl,
+            is_first_last_piece_priority=torrent.f_l_piece_prio,
+            tags=torrent.tags,
+            ratio_limit=torrent.ratio_limit,
+            seeding_time_limits=torrent.seeding_time_limit
+    )
+    print()
